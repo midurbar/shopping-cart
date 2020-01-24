93785027417dedc0a72a4d61074bfc9e9c382904
midurbar <60234563+midurbar@users.noreply.github.com>
2020-01-24 20:21:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,27 @@
  * @param {array de objetos} carrito 
  */
 export function calcularTotal(carrito) {
-  return 0;
+  var total= 0;
+  for (var i=0;i<carrito.length;i++) {
+    if (carrito[i].cantidad) {
+      total+=carrito[i].precio*carrito[i].cantidad;
+    }else {
+      total+=carrito[i].precio;
+    }
+  }
+  return total;
+}
+
+export function calcularPeso(carrito) {
+  var totalp= 0;
+  for (var i=0;i<carrito.length;i++) {
+    if (carrito[i].cantidad) {
+      totalp+=carrito[i].peso*carrito[i].cantidad;
+    }else {
+      totalp+=carrito[i].peso;
+    }
+  }
+  return totalp;
 }
 
 
